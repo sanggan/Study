cbeb441e7c4a6bfc13bce4ee5f987ff5d8635227
sanggan <sttv4303@naver.com>
2020-09-05 16:01:51 +0900

    20/09/03

--- kakaoTest/src/kakaoTest/ploblem01.java
+++ kakaoTest/src/kakaoTest/ploblem01.java
@@ -44,7 +44,7 @@ public class ploblem01 {
 	        }
 	        return answer;
 	   }
-	public static void main(String[] args) {
+	public static void maint(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println(solution("abcabcabcabcdededededede"));
 	}
